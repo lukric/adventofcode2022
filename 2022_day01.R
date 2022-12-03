bf2b24e307a2a814f21f3a7869784db7ef7e5ba1
lukric <lukasrichter@gmx.at>
2022-12-03 08:15:32 +0100

    tidy code

--- 2022_day01.R
+++ 2022_day01.R
@@ -6,28 +6,28 @@ library("dplyr")
 
 # PART 1
 # read data
-my_input <- read.delim("data/day1_elves.txt", blank.lines.skip = FALSE, header = FALSE)
+elves_inventory <- read.delim("data/day1_elves.txt",
+                              blank.lines.skip = FALSE,
+                              header = FALSE)
 
 # ugly code
-x <- my_input$V1
+x <- elves_inventory$V1
 
 na_idx <- c(1, which(is.na(x)), length(x) + 1)
 na_idx_length <- diff(na_idx)
 
-my_fct <- rep(1:(length(na_idx) - 1), na_idx_length)
+elves_inventory$elve_id <- rep(1:(length(na_idx) - 1), na_idx_length)
 
-my_input$elve_id <- my_fct
-
-my_smry <- my_input |> 
+elves_cals <- elves_inventory |> 
   dplyr::filter(!is.na(V1)) |> 
   dplyr::group_by(elve_id) |> 
   dplyr::summarise(sum = sum(V1))
 
 # result
-max(my_smry$sum)
+max(elves_cals$sum)
 
 # PART 2
-my_smry |> 
+elves_cals |> 
   dplyr::arrange(dplyr::desc(sum)) |> 
   dplyr::slice(1:3) |> 
   dplyr::pull(sum) |> 
